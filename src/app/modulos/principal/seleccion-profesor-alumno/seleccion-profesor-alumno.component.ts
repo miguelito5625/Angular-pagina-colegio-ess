7b30045222570bae9e3184b298a6e6758115cd49
exbinario <miguel.4to.1994@gmail.com>
2022-08-11 08:40:59 -0600

    jugando con las animaciones

--- src/app/modulos/principal/seleccion-profesor-alumno/seleccion-profesor-alumno.component.ts
+++ src/app/modulos/principal/seleccion-profesor-alumno/seleccion-profesor-alumno.component.ts
@@ -1,6 +1,7 @@
 import { animation } from '@angular/animations';
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ENTRANCES_ANIMATIONS } from 'src/app/extra-data/entrances-animatios';
+import { EXITS_ANIMATIONS } from 'src/app/extra-data/exits-animatios';
 
 @Component({
   selector: 'app-seleccion-profesor-alumno',
@@ -9,8 +10,8 @@ import { ENTRANCES_ANIMATIONS } from 'src/app/extra-data/entrances-animatios';
 })
 export class SeleccionProfesorAlumnoComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  @ViewChild('opcionProfesor', {static: false}) opcionProfesor: ElementRef;
-  @ViewChild('opcionAlumno', {static: false}) opcionAlumno: ElementRef;
+  @ViewChild('opcionProfesor', { static: false }) opcionProfesor: ElementRef;
+  @ViewChild('opcionAlumno', { static: false }) opcionAlumno: ElementRef;
 
   animacionEntrada1: String = "";
   animacionEntrada2: String = "";
@@ -18,8 +19,8 @@ export class SeleccionProfesorAlumnoComponent implements OnInit, AfterViewInit, 
   animationendListener;
 
   constructor(
-    private renderer:Renderer2
-  ) { 
+    private renderer: Renderer2
+  ) {
 
     this.animacionEntrada1 = this.obtenerAnimacionEntrada();
     this.animacionEntrada2 = this.obtenerAnimacionEntrada();
@@ -29,32 +30,38 @@ export class SeleccionProfesorAlumnoComponent implements OnInit, AfterViewInit, 
   ngOnInit(): void {
     // const animacion = this.obtenerAnimacionEntrada();
     // console.log(animacion);
-    
+
   }
 
-  obtenerAnimacionEntrada(): String {
-    return ENTRANCES_ANIMATIONS[Math.floor(Math.random()*ENTRANCES_ANIMATIONS.length)];
+  obtenerAnimacionEntrada(): string {
+    return ENTRANCES_ANIMATIONS[Math.floor(Math.random() * ENTRANCES_ANIMATIONS.length)];
+  }
+
+  obtenerAnimacionSalida(): string {
+    return EXITS_ANIMATIONS[Math.floor(Math.random() * EXITS_ANIMATIONS.length)];
   }
 
   ngAfterViewInit(): void {
     this.animationendListener = this.renderer.listen(this.opcionAlumno.nativeElement, 'animationend', (evt) => {
       console.log("TERMINO ANIMACION");
-     });
+    });
   }
 
   ngOnDestroy(): void {
     this.animationendListener.unsubscribe();
   }
-  
+
 
   public navegarSiguiente(opcion: String): void {
     console.log(opcion);
     if (opcion === "profesor") {
-    this.renderer.removeClass(this.opcionAlumno.nativeElement, 'animate__zoomIn');
-    this.renderer.addClass(this.opcionAlumno.nativeElement, 'animate__bounceOutLeft');
+      const className = this.opcionAlumno.nativeElement.className.split(' ')[1];
+      this.renderer.removeClass(this.opcionAlumno.nativeElement, className);
+      this.renderer.addClass(this.opcionAlumno.nativeElement, this.obtenerAnimacionSalida());
     } else {
-    this.renderer.removeClass(this.opcionProfesor.nativeElement, 'animate__zoomIn');
-    this.renderer.addClass(this.opcionProfesor.nativeElement, 'animate__bounceOutLeft');
+      const className = this.opcionProfesor.nativeElement.className.split(' ')[1];
+      this.renderer.removeClass(this.opcionProfesor.nativeElement, className);
+      this.renderer.addClass(this.opcionProfesor.nativeElement, this.obtenerAnimacionSalida());
     }
   }
 
