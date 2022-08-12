6d792d55833a0f745bdcbffee011991b003481ed
exbinario <miguel.4to.1994@gmail.com>
2022-08-12 10:02:56 -0600

    se esta trabajando la seleccion de grado

--- src/app/modulos/principal/seleccion-grado/seleccion-grado.component.ts
+++ src/app/modulos/principal/seleccion-grado/seleccion-grado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ENTRANCES_ANIMATIONS } from 'src/app/extra-data/entrances-animatios';
 
 @Component({
   selector: 'app-seleccion-grado',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeleccionGradoComponent implements OnInit {
 
-  constructor() { }
+  grados:any[] = [
+    {
+      id: 1,
+      nombre: "Primero Básico"
+    },
+    {
+      id: 2,
+      nombre: "Segundo Básico"
+    },
+    {
+      id: 1,
+      nombre: "Tercero Básico"
+    },
+    
+  ];
+
+  
+  constructor() { 
+
+    for(let i = 0; i < this.grados.length; i++) {
+      this.grados[i].animacionEntrada = this.obtenerAnimacionEntrada();
+      console.log(this.grados[i]);
+    }
+
+  }
 
   ngOnInit(): void {
+  }
+
+  obtenerAnimacionEntrada(): string {
+    return ENTRANCES_ANIMATIONS[Math.floor(Math.random() * ENTRANCES_ANIMATIONS.length)];
+  }
+
+  siguientePagina(grado: object): void {
+    console.log(grado);
   }
 
 }
